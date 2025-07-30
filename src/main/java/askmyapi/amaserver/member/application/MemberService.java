42439255c66852a6716bb90972b42849e81f8e55
seock1000 <seock1000@github.com>
2025-07-30 21:36:11 +0900

    [FEAT] define createMember interface

--- src/main/java/askmyapi/amaserver/member/application/MemberService.java
+++ src/main/java/askmyapi/amaserver/member/application/MemberService.java
@@ -1,9 +1,19 @@
 package askmyapi.amaserver.member.application;
 
+import askmyapi.amaserver.member.application.port.in.CreateMemberCommand;
+import askmyapi.amaserver.member.application.port.in.CreateMemberUseCase;
+import askmyapi.amaserver.member.application.port.in.MemberResult;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
-public class MemberService {
+@Transactional
+public class MemberService implements CreateMemberUseCase {
+
+    @Override
+    public MemberResult.Create createMember(CreateMemberCommand command) {
+        return null;
+    }
 }
--- src/main/java/askmyapi/amaserver/member/application/port/in/CreateMemberUseCase.java
+++ src/main/java/askmyapi/amaserver/member/application/port/in/CreateMemberUseCase.java
@@ -1,4 +1,6 @@
 package askmyapi.amaserver.member.application.port.in;
 
 public interface CreateMemberUseCase {
+
+    MemberResult.Create createMember(CreateMemberCommand command);
 }
